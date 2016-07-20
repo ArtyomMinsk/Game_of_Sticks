9699de232a7ee21ab55468b6ab893d44bcbbb76f
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-19 23:41:07 -0400

    Added tests for valid amount of sticks on the table and valid input from the player

--- sticks_test.py
+++ sticks_test.py
@@ -1,16 +1,33 @@
-from sticks import number_of_original_input_sticks_check
-from sticks import number_of_input_sticks_from_player_check
+from sticks import valid_sticks_check
+from sticks import valid_input_from_player
+from sticks import update_num_sticks
+from sticks import no_sticks_left
 
-def test_number_of_original_sticks_input():
+def test_valid_sticks_check():
+    input_0 = 0
     input_1 = 50
     input_2 = 150
 
-    assert number_of_original_input_sticks_check(input_1) == True
-    assert number_of_original_input_sticks_check(input_2) == False
+    assert valid_sticks_check(input_0) == False
+    assert valid_sticks_check(input_1) == True
+    assert valid_sticks_check(input_2) == False
 
-def test_number_of_sticks_input():
+def test_valid_input_from_player():
+    input_0 = 0
     input_1 = 2
     input_2 = 5
 
-    assert number_of_input_sticks_from_player_check(input_1) == True
-    assert number_of_input_sticks_from_player_check(input_2) == False
+    assert valid_input_from_player(input_0) == False
+    assert valid_input_from_player(input_1) == True
+    assert valid_input_from_player(input_2) == False
+
+def test_update_num_sticks():
+    sticks_left = 5
+    player_sticks = 3
+
+    assert update_num_sticks(sticks_left, player_sticks) == 2
+
+def test_no_sticks_left():
+    assert no_sticks_left(0) == True
+    assert no_sticks_left(-1) == True
+    assert no_sticks_left(1) == False
