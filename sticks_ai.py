305236d7a5f2e51c89726412c77cde41ed6656b9
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-21 00:36:40 -0400

    Added two functions(AI wi/lose) that update AI's data structure

--- sticks_ai.py
+++ sticks_ai.py
@@ -5,7 +5,6 @@ def valid_sticks_check(original_number_sticks):
         print("Out of range! Please enter a number between 10 and 100.")
         return False
     else:
-        #print("in range")
         return True
 
 def amount_of_sticks_input():
@@ -21,7 +20,6 @@ def valid_input_from_player(player_input):
         print("Out of range! Please enter a number between 1 and 3.")
         return False
     else:
-        #print("in range")
         return True
 
 def player_takes_out_sticks(player):
@@ -71,8 +69,6 @@ def two_players_game(original_number_sticks):
 def init_ai_list(original_number_sticks):
     ai_list = []
 
-    # for loop from 0 to original_number_sticks - 1
-    # if original_number_sticks=10, from 0 - 9
     for index in range(1, original_number_sticks):
         tup = (index, [1, 2, 3], [])
         ai_list.append(tup)
@@ -81,43 +77,66 @@ def init_ai_list(original_number_sticks):
 
 def ai_takes_out_sticks(ai_list, sticks_left):
     tup = ai_list[sticks_left - 1]
-    print("tup = {}".format(tup))
+    #print("tup = {}".format(tup))
 
     content = tup[1]
-    print("content = {}".format(content))
+    #print("content = {}".format(content))
     beside = tup[2]
-    print("beside = {}".format(beside))
+    #print("beside = {}".format(beside))
 
     random_index = randrange(0, len(content))
-    print("random_index = {}".format(random_index))
+    #print("random_index = {}".format(random_index))
 
     random_choice = content[random_index]
-    print("random_choice = {}".format(random_choice))
+    print("AI selects {}".format(random_choice))
 
     content.pop(random_index)
     beside.append(random_choice)
 
     tup_updated = (tup[0], content, beside)
     ai_list[sticks_left - 1] = tup_updated
+    #print("ai_list", *ai_list, sep='\n')
 
     return (ai_list, random_choice)
 
-# This function will updated the ai_list when the AI wins.
-# Function must look through each entry in ai_list, and check beside [].
-# If beside is NOT empty, move the contents of beside [] back into content [] twice.
-# Remove entry from beside [].
+# This function updates the ai_list when the AI wins.
 def update_win_ai_list(ai_list):
     updated_ai_list = ai_list
 
+    for item in range(len(updated_ai_list)):
+        tup = updated_ai_list[item]
+        content = tup[1]
+        beside = tup[2]
+        if beside != []:
+            # print("content before append: ", content)
+            # print("beside before pop: ", beside)
+            content.append(beside[0])
+            content.append(beside[0])
+            beside.pop(0)
+    # print("content_append: ", content)
+    # print("beside after pop: ", beside)
+    #print("updated_ai_list after: ", *updated_ai_list, sep='\n')
     return updated_ai_list
 
-# This function will update the ai_list when the AI looses.
-# Function must look through each entry in ai_list and check beside [].
-# If beside is NOT empty, then check if beside entry is inside content [].
-# If yes, drop beside entry. If no, put it back into content [].
+# This function updates the ai_list when the AI looses.
 def update_loose_ai_list(ai_list):
     updated_ai_list = ai_list
 
+    for item in range(len(updated_ai_list)):
+        tup = updated_ai_list[item]
+        content = tup[1]
+        beside = tup[2]
+        # print("updated_ai_list: ", *updated_ai_list, sep='\n')
+        # print("content: ", content)
+        # print("beside: ", beside)
+
+        if beside != []:
+            if beside[0] not in content:
+                #print("content before append: ", content)
+                content.append(beside[0])
+            beside.pop(0)
+
+    #print("updated_ai_list after: ", *updated_ai_list, sep='\n')
     return updated_ai_list
 
 def player_vs_ai_game(original_number_sticks, hat_list):
@@ -131,26 +150,19 @@ def player_vs_ai_game(original_number_sticks, hat_list):
 
         if no_sticks_left(total_sticks_left):
             print("Human, you lose.")
-            return ai_list
-            #REPLACE WITH
-            #return update_win_ai_list(ai_list)
-            #
+            #print("ai_list after human lost: ", *ai_list, sep='\n')
+            return update_win_ai_list(ai_list)
 
         # This function returns (updated_ai_list, ai_takes_sticks)
         ai_return = ai_takes_out_sticks(ai_list, total_sticks_left)
-        print(ai_return)
-
         total_sticks_left -= ai_return[1]
         print("There are {} sticks left.".format(total_sticks_left))
 
         if no_sticks_left(total_sticks_left):
             print("AI, you lose.")
-            return ai_list
-            #REPLACE WITH
-            #return update_loose_ai_list(ai_return[0])
-            #
+            #print("ai_list after AI lost: ", *ai_return[0], sep='\n')
+            return update_loose_ai_list(ai_return[0])
 
-# This is the main function where Game starts
 def main():
     print("Welcome to the Game of Sticks!")
 
@@ -162,7 +174,7 @@ def main():
         option = int(input("Please input (1) or (2) only: "))
 
     ai_hat_list = init_ai_list(num_sticks)
-    print("Initial AI list is: {}".format(ai_hat_list))
+    #print("Initial AI list is: ", *ai_hat_list, sep='\n')
 
     while True:
         if option == 1:
